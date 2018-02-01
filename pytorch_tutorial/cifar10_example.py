43341746d4a4a54a1a97bc538c0e3611d24adf1e
jvitorfromhell <jvitor.schneid@gmail.com>
2018-01-31 22:11:18 -0200

    PyTorch Tutorial 2

--- pytorch_tutorial/cifar10_example.py
+++ pytorch_tutorial/cifar10_example.py
@@ -2,6 +2,12 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.optim as optim
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,9 +32,88 @@ def imshow(img):
 	img = img / 2 + 0.5
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
+	#plt.show()
 
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
 
 imshow(torchvision.utils.make_grid(images))
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# Convolutional Neural Network Definition
+class Net(nn.Module):
+	def __init__(self):
+		super(Net, self).__init__()
+		self.conv1 = nn.Conv2d(3, 6, 5)
+		self.pool = nn.MaxPool2d(2, 2)
+		self.conv2 = nn.Conv2d(6, 16, 5)
+		self.fc1 = nn.Linear(16 * 5 * 5, 120)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
+
+	def forward(self, x):
+		x = self.pool(F.relu(self.conv1(x)))
+		x = self.pool(F.relu(self.conv2(x)))
+		x = x.view(-1, 16 * 5 * 5)
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+		return x
+
+# Initialize net
+net = Net()
+net.cuda()
+
+# Loss Function and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Training
+for epoch in range(2):
+	running_loss = 0.0
+
+	for i, data in enumerate(trainloader, 0):
+		inputs, labels = data
+		inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+
+		optimizer.zero_grad()
+
+		outputs = net(inputs)
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+
+		running_loss += loss.data[0]
+		if i % 2000 == 1999:
+			print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/ 2000))
+			running_loss = 0.0
+
+print('Training finished')
+
+# Visualizing Test Image Samples
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# Predicting Values and Checking Against Ground Truth
+outputs = net(Variable(images).cuda())
+
+_, predicted = torch.max(outputs.data, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+# Checking Accuracy on Whole Test Set
+correct = 0
+total = 0
+
+for data in testloader:
+	images, labels = data
+	labels = Variable(labels).cuda()
+	outputs = net(Variable(images).cuda())
+	_, predicted = torch.max(outputs.data, 1)
+	total += labels.size(0)
+	correct += (predicted == labels.data).sum()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
